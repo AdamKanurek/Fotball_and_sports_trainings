0c88890c5ea9481b7fb75f686d130b701d289699
Adam <adam.kanurek@icloud.com>
2025-06-20 20:39:34 +0200

    přidaní privacy policy

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
-    // Scroll reveal efekt
-    const sections = document.querySelectorAll('section');
-    const revealOnScroll = () => {
-      const trigger = window.innerHeight * 0.85;
-      sections.forEach((section) => {
-        const top = section.getBoundingClientRect().top;
-        if (top < trigger) {
-          section.classList.add('reveal');
-        }
-      });
-    };
-  
-    window.addEventListener('scroll', revealOnScroll);
-    revealOnScroll(); // spustit i při načtení
-  });
-  
+
+  // Scroll reveal efekt
+  const sections = document.querySelectorAll('section');
+  const revealOnScroll = () => {
+    const trigger = window.innerHeight * 0.85;
+    sections.forEach((section) => {
+      const top = section.getBoundingClientRect().top;
+      if (top < trigger) {
+        section.classList.add('reveal');
+      }
+    });
+  };
+
+  const actualYear = document.getElementById("year");
+  if (actualYear) {
+    actualYear.textContent = new Date().getFullYear();
+  }
+
+  window.addEventListener('scroll', revealOnScroll);
+  revealOnScroll(); // spustit i při načtení
+});
